397d3346ad08cc8a7ede8fe3cf8032d0a4515cc3
Prabhat <prabhat1323@gmail.com>
2021-12-23 15:06:48 +0530

    some responsive chages

--- js/index.js
+++ js/index.js
@@ -3,13 +3,26 @@ $(document).ready(function(){
        if($(this).attr("class")=="fa fa-angle-right"){
            $(this).removeClass("fa fa-angle-right");
            $(this).addClass("fa fa-angle-left");
-           $("#menu-left").css({width:'250px'});
+           $("#menu-left").css({width:'250px',padding:'15px'});
 
        }
        else{
             $(this).removeClass("fa fa-angle-left");
             $(this).addClass("fa fa-angle-right");
-            $("#menu-left").css({width:'0px'});
+            $("#menu-left").css({width:'0px',padding:'0px'});
        }
     });
+
+    $(".nav-item",".menu-left").each(function(){
+        $(this).click(function(){
+            $("#menu-icon").removeClass("fa fa-angle-left");
+            $("#menu-icon").addClass("fa fa-angle-right");
+            $("#menu-left").css({width:'0px',padding:'0px'});
+        });
+    });
+
+    // window.onresize = function(){
+    //     let menu = document.querySelector("#menu-left");
+    //     console.log(menu);
+    // }
 });
