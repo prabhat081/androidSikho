7c9e9c7520c10e08dac01df4a1ecdb3d22d26103
Prabhat <prabhat1323@gmail.com>
2021-12-24 12:10:20 +0530

    sidebar change

--- js/index.js
+++ js/index.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
        if($(this).attr("class")=="fa fa-angle-right"){
            $(this).removeClass("fa fa-angle-right");
            $(this).addClass("fa fa-angle-left");
-           $("#menu-left").css({width:'250px',padding:'15px'});
-
+           $("#menu-left").css({width:'200px',padding:'15px'});
+    
        }
        else{
             $(this).removeClass("fa fa-angle-left");
             $(this).addClass("fa fa-angle-right");
             $("#menu-left").css({width:'0px',padding:'0px'});
+            resize();
        }
     });
 
@@ -20,4 +21,18 @@ $(document).ready(function(){
             $("#menu-left").css({width:'0px',padding:'0px'});
         });
     });
+
+   
 });
+
+function resize(){
+    window.onresize = function(){ 
+       let menu_left =  document.querySelector("#menu-left");
+       console.log(getComputedStyle(menu_left).getPropertyValue('width'));
+       if(getComputedStyle(menu_left).getPropertyValue('width')=='0px'){
+            $("#menu-left").css({width:'200px',padding:'15px'});
+            $("#menu-icon").removeClass("fa fa-angle-right");
+            $("#menu-icon").addClass("fa fa-angle-left");
+       }
+    }
+}
